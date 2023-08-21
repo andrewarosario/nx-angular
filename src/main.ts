a0d18da05e38c272b73dd8e6a476173983c01231
andrewarosario <andrew.arosario@gmail.com>
2023-08-21 19:56:48 -0300

    fix: remove console

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { throwError } from 'rxjs';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+  .catch((err) => throwError(() => err));
